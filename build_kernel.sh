e8181c7e016efb9ee4e7aa5c1a603617565c49b8
Christopher A. Williamson <home@chrisaw.com>
2021-04-09 20:39:46 +0100

    Adapt build kernel script for docker usage

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
 export ARCH=arm64
-mkdir out
+mkdir -p out
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
-CLANG_TRIPLE=aarch64-linux-gnu-
+ANDROID_MAJOR_VERSION="r"
+PLATFORM_VERSION=11
+export ANDROID_MAJOR_VERSION PLATFORM_VERSION
+
+BUILD_CROSS_COMPILE="/opt/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+KERNEL_LLVM_BIN="/opt/qcom-clang-8/bin/clang"
+CLANG_TRIPLE="aarch64-linux-gnu-"
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/c2q_chn_openx_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
- 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+make -C $(pwd) \
+  O=$(pwd)/out \
+  $KERNEL_MAKE_ENV \
+  CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+  REAL_CC=$KERNEL_LLVM_BIN \
+  CLANG_TRIPLE=$CLANG_TRIPLE \
+  vendor/c2q_chn_openx_defconfig
+
+make -j$(nproc --all) -C $(pwd) \
+  O=$(pwd)/out \
+  $KERNEL_MAKE_ENV \
+  CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+  REAL_CC=$KERNEL_LLVM_BIN \
+  CLANG_TRIPLE=$CLANG_TRIPLE
+
+cp -v \
+  out/arch/arm64/boot/Image \
+  $(pwd)/arch/arm64/boot/Image
